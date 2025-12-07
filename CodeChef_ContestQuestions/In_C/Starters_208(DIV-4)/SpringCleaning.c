96a771b6b40067b30ee65f02bb8aa344477f60f7
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-12-07 19:24:06 +0530

    1)Spring Cleaning (Difficulty:179)
    Chef is now performing a thorough cleaning of every room in his house.
    
    Chef's house has 
    X
    X small rooms and 
    Y
    Y large rooms.
    Each small room requires 
    30
    30 minutes to clean, and each large room requires 
    60
    60 minutes.
    
    How many minutes does Chef need to clean his entire house?
    
    Input Format
    The only line of input contains two space-separated integers 
    X
    X and 
    Y
    Y — the number of small and large rooms in Chef's house, respectively.
    Output Format
    Output a single integer: the number of minutes Chef requires to clean his house.
    
    Constraints
    1
    ≤
    X
    ,
    Y
    ≤
    10
    1≤X,Y≤10
    Sample 1:
    Input
    Output
    4 3
    300
    Explanation:
    There are 
    4
    4 small rooms, each needing 
    30
    30 minutes to clean. The total time needed for the small rooms is hence 
    4
    ×
    30
    =
    120
    4×30=120 minutes.
    There are 
    3
    3 large rooms, each needing 
    60
    60 minutes to clean. The total time needed for the large rooms is hence 
    3
    ×
    60
    =
    180
    3×60=180 minutes.
    The total time needed for all rooms is 
    120
    +
    180
    =
    300
    120+180=300 minutes.
    
    Sample 2:
    Input
    Output
    2 10
    660
    Explanation:
    There are 
    2
    2 small rooms, each needing 
    30
    30 minutes to clean. The total time needed for the small rooms is hence 
    2
    ×
    30
    =
    60
    2×30=60 minutes.
    There are 
    10
    10 large rooms, each needing 
    60
    60 minutes to clean. The total time needed for the large rooms is hence 
    10
    ×
    60
    =
    600
    10×60=600 minutes.
    The total time needed for all rooms is 
    60
    +
    600
    =
    660
    60+600=660 minutes.
    
    Sample 3:
    Input
    Output
    7 3
    390

---
+++ CodeChef_ContestQuestions/In_C/Starters_208(DIV-4)/SpringCleaning.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+
+
+    int X,Y;
+    scanf("%d %d",&X,&Y);
+    
+    int time=X*30 + Y*60;
+    
+    printf("%d\n",time);
+    
+    return 0;
+}
+
