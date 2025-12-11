12b0ae95db475337a5cd61aa740f5f9febf27c55
Prakhar Pandey <studiousprakhar2260@gmail.com>
2025-12-11 20:06:34 +0530

    Update Entertainments.c

--- CodeChef_ContestQuestions/In_C/Starters_216(DIV-4)/Entertainments.c
+++ CodeChef_ContestQuestions/In_C/Starters_216(DIV-4)/Entertainments.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+	// your code goes here
+	int N;
+	scanf("%d",&N);
+	
+	if(N<5)
+	 {
+	   printf("%d",200 * N);  
+	 }
+	 
+	 else
+     {
+        printf("1000"); 
+     }
+     
+     return 0;
+}
+
 
